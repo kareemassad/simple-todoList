1e79b8a85c8da43840c5eb0a4c0120da8071de6d
Kareem Assad <kareemassad5@gmail.com>
2020-03-02 23:26:00 -0500

    all functionality done, kinda empty

--- todo.py
+++ todo.py
@@ -11,7 +11,7 @@ root.configure(bg="white")
 # Change Title
 root.title("My To-Do List")
 # Change window size
-root.geometry("200x500")
+root.geometry("325x275")
 
 # Create an empty list
 tasks = []
@@ -34,6 +34,8 @@ def add_task():
         update_listbox()
     else:
         label_display["text"] = "Please enter a task."
+    
+    text_input.delete(0,"end")
 
 def del_all():
     global tasks
@@ -41,7 +43,12 @@ def del_all():
     update_listbox()
 
 def del_one():
-    pass
+    #get the item selected
+    task = listBox_tasks.get("active")
+    #confirm it exists in list
+    if task in tasks:
+        tasks.remove(task)
+    update_listbox()
 
 def sort_asc():
     tasks.sort()
@@ -60,44 +67,44 @@ def show_tasks_count():
     count = len(tasks)
     msg = "Number of Tasks %s" %count
     label_display["text"] = msg
-    
 
+# GUI
 label_title = tkinter.Label(root, text="TO-DO-List", bg="white")
-label_title.pack()
+label_title.grid(row=0,column=0)
 
 label_display = tkinter.Label(root, text="", bg="white")
-label_display.pack()
+label_display.grid(row=0,column=1)
 
 text_input = tkinter.Entry(root, width=15)
-text_input.pack()
+text_input.grid(row=1,column=1)
 
-# buttons
+# Buttons
 
 btn_add_task = tkinter.Button(root, text="Add task", fg="green", bg="white", command=add_task)
-btn_add_task.pack()
+btn_add_task.grid(row=1,column=0)
 
 btn_del_all = tkinter.Button(root, text="Delete All", fg="green", bg="white", command=del_all)
-btn_del_all.pack()
+btn_del_all.grid(row=2,column=0)
 
 btn_del_one = tkinter.Button(root, text="Delete One", fg="green", bg="white", command=del_one)
-btn_del_one.pack()
+btn_del_one.grid(row=3,column=0)
 
 btn_sort_asc = tkinter.Button(root, text="Sort Ascending", fg="green", bg="white", command=sort_asc)
-btn_sort_asc.pack()
+btn_sort_asc.grid(row=4,column=0)
 
 btn_sort_desc = tkinter.Button(root, text="Sort Descending", fg="green", bg="white", command=sort_desc)
-btn_sort_desc.pack()
+btn_sort_desc.grid(row=5,column=0)
 
 btn_random = tkinter.Button(root, text="Choose Random", fg="green", bg="white", command=random)
-btn_random.pack()
+btn_random.grid(row=6,column=0)
 
 btn_show_tasks_count= tkinter.Button(root, text="Show All Tasks", fg="green", bg="white", command=show_tasks_count)
-btn_show_tasks_count.pack()
+btn_show_tasks_count.grid(row=7,column=0)
 
 btn_exit = tkinter.Button(root, text="Exit", fg="green", bg="white", command=exit)
-btn_exit.pack()
+btn_exit.grid(row=8,column=0)
 
 listBox_tasks = tkinter.Listbox(root)
-listBox_tasks.pack()
+listBox_tasks.grid(row=2, column=1, rowspan=7)
 #start the main event loop
 root.mainloop()
